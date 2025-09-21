49803b75e28ddd93ef9b811ba97209984f925012
ArnoldoDev <aoxlajo@miumg.edu.gt>
2025-09-21 09:47:15 -0600

    Se crea el Login

--- AppIA/views.py
+++ AppIA/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'registro.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # registrar usuario
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password2'])
+                user.save()
+                login(request, user)
+                return HttpResponse('Usuario creado satisfactoriamente')
+            except IntegrityError:
+                return render(request, 'registro.html', {
+                    'form': UserCreationForm,
+                    "error": 'El usuario ya existe'
+                })
+        return render(request, 'registro.html', {
+            'form': UserCreationForm,
+            "error": 'Las contraseñas no coinciden'
+        })
